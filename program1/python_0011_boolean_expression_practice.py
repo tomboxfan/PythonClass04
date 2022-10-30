db0aa68887f72811a08f1d3a437d4aa5ffa45a3e
Tom Fan <tomboxfan@hotmail.com>
2022-10-30 11:35:41 +0800

    lesson 3

---
+++ program1/python_0011_boolean_expression_practice.py
@@ -0,0 +1,46 @@
+'''
+
+Let's say, there are 34 students in your class.
+18 boys, 16 girls.
+'''
+
+boys = 18
+girls = 16
+total = boys + girls
+
+print(f"We have boys: ", boys)
+print(f"We have girls: ", girls)
+print(f"We have total students: ", total)
+
+'''
+Now, please express the following statement using boolean expression, and print its boolean value.
+I will finish the 1st one for you.
+'''
+
+'''
+Q1) We have more than 20 boys, right?
+'''
+we_have_more_than_20_boys = (boys > 20)
+print("We have more than 20 boys, right? ", we_have_more_than_20_boys)
+
+
+
+'''
+Q2) We have more than 16 girls, right?
+'''
+
+'''
+Q3) We have equal boys and girls, right?
+'''
+
+'''
+Q4) We have more boys than girls, right?
+'''
+
+'''
+Q5) there is more than 5 boys than girls, right? 
+'''
+
+
+
+
